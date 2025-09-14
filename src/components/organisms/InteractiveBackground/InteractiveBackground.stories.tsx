a5b5eac9e919c3738e48f42dff86435d907b4eb3
krespokiller <69087322+krespokiller@users.noreply.github.com>
2025-09-13 23:11:07 -0500

    fix: update import statement for InteractiveBackground to named import

--- src/components/organisms/InteractiveBackground/InteractiveBackground.stories.tsx
+++ src/components/organisms/InteractiveBackground/InteractiveBackground.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import InteractiveBackground from './InteractiveBackground';
+import { InteractiveBackground } from './InteractiveBackground';
 
 const meta: Meta<typeof InteractiveBackground> = {
   title: 'Organisms/InteractiveBackground',
